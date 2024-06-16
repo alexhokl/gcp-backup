770e412a84e1d2cc31cfec40fc0894c28806e46c
alexhokl <alexhokl@gmail.com>
2024-06-16 11:44:46 +0800

    Enhanced error messages

--- cmd/run.go
+++ cmd/run.go
@@ -38,7 +38,7 @@ func runBackup(_ *cobra.Command, _ []string) error {
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to create storage client: %w", err)
 	}
 	bucketName := viper.GetString("bucket")
 	machineAlias := viper.GetString("machine_alias")
@@ -46,19 +46,19 @@ func runBackup(_ *cobra.Command, _ []string) error {
 	paths := viper.GetStringSlice("paths")
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to retrieve home directory: %w", err)
 	}
 	localFilePaths, err := ExtractFilePathsFromList(homeDir, paths)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to extract local file paths: %w", err)
 	}
 	checksums, err := GetChecksums(localFilePaths)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to generate checksums of local files: %w", err)
 	}
 	localPathsRequiredBackup, ignoredPaths, err := GetPathsRequiredBackup(ctx, client, bucketName, machineAlias, homeDir, checksums)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to determine paths required backup: %w", err)
 	}
 	if len(ignoredPaths) > 0 {
 		for _, p := range ignoredPaths {
@@ -69,11 +69,11 @@ func runBackup(_ *cobra.Command, _ []string) error {
 		for _, localPath := range localPathsRequiredBackup {
 			relativePath, err := filepath.Rel(homeDir, localPath)
 			if err != nil {
-				return err
+				return fmt.Errorf("unable to determine relative path of local file [%s]: %w", localPath, err)
 			}
 			if !runBackupOpts.dryRun {
 				if err := CopyFileToBucket(ctx, client, bucketName, machineAlias, relativePath, localPath); err != nil {
-					return err
+					return fmt.Errorf("unable to upload file [%s] to bucket [gs://%s/%s/%s]: %w", localPath, bucketName, machineAlias, relativePath, err)
 				}
 			}
 			fmt.Printf("Copied file [%s] to bucket [gs://%s/%s/%s]\n", localPath, bucketName, machineAlias, relativePath)
@@ -86,16 +86,16 @@ func runBackup(_ *cobra.Command, _ []string) error {
 func CopyFileToBucket(ctx context.Context, storageClient *storage.Client, bucketName string, machineAlias string, relativePath string, localPath string) error {
 	localFileBytes, err := iohelper.ReadBytesFromFile(localPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to read local file [%s]: %w", localPath, err)
 	}
 
 	objectPath := path.Join(machineAlias, "/", relativePath)
 	writer := storageClient.Bucket(bucketName).Object(objectPath).NewWriter(ctx)
 	if _, err := writer.Write(localFileBytes); err != nil {
-		return err
+		return fmt.Errorf("unable to write to object [%s]: %w", objectPath, err)
 	}
 	if err := writer.Close(); err != nil {
-		return err
+		return fmt.Errorf("unable to complete writing object [%s]: %w", objectPath, err)
 	}
 	return nil
 }
@@ -106,12 +106,12 @@ func GetPathsRequiredBackup(ctx context.Context, storageClient *storage.Client, 
 	for localPath, localChecksum := range checksums {
 		relativePath, err := filepath.Rel(parentPath, localPath)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, fmt.Errorf("unable to determine relative path of local file [%s]: %w", localPath, err)
 		}
 		objectPath := path.Join(machineAlias, "/", relativePath)
 		required, err := IsBackupRequired(ctx, storageClient, bucketName, objectPath, localChecksum)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, fmt.Errorf("unable to determine if backup is required for file [%s]: %w", localPath, err)
 		}
 		if required {
 			requiredPaths = append(requiredPaths, localPath)
@@ -125,14 +125,14 @@ func GetPathsRequiredBackup(ctx context.Context, storageClient *storage.Client, 
 func IsBackupRequired(ctx context.Context, storageClient *storage.Client, bucketName string, objectPath string, localChecksum uint32) (bool, error) {
 	exist, err := IsObjectExist(ctx, storageClient, bucketName, objectPath)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("unable to determine if object [%s] exists in bucket [gs://%s]: %w", objectPath, bucketName, err)
 	}
 	if !exist {
 		return true, nil
 	}
 	remoteChecksum, err := GetObjectChecksum(ctx, storageClient, bucketName, objectPath)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("unable to retrieve checksum of object [%s] in bucket [gs://%s]: %w", objectPath, bucketName, err)
 	}
 	return localChecksum != remoteChecksum, nil
 }
@@ -143,7 +143,7 @@ func GetChecksums(localPaths []string) (map[string]uint32, error) {
 	for _, localPath := range localPaths {
 		checksum, err := iohelper.GenerateCRC32Checksum(localPath)
 		if err != nil {
-			errs = append(errs, err)
+			errs = append(errs, fmt.Errorf("unable to generate checksum of file [%s]: %w", localPath, err))
 		} else {
 			checksums[localPath] = checksum
 		}
@@ -160,7 +160,7 @@ func IsObjectExist(ctx context.Context, storageClient *storage.Client, bucketNam
 		if err == storage.ErrObjectNotExist {
 			return false, nil
 		}
-		return false, err
+		return false, fmt.Errorf("unable to retrieve attributes of object [%s] in bucket [gs://%s]: %w", objectPath, bucketName, err)
 	}
 	return true, nil
 }
@@ -168,7 +168,7 @@ func IsObjectExist(ctx context.Context, storageClient *storage.Client, bucketNam
 func GetObjectChecksum(ctx context.Context, storageClient *storage.Client, bucketName string, objectPath string) (uint32, error) {
 	attrs, err := storageClient.Bucket(bucketName).Object(objectPath).Attrs(ctx)
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("unable to retrieve attributes of object [%s] in bucket [gs://%s]: %w", objectPath, bucketName, err)
 	}
 	return attrs.CRC32C, nil
 }
@@ -178,7 +178,7 @@ func GetRelativePaths(parentPath string, localPaths []string) ([]string, error) 
 	for i, localPath := range localPaths {
 		p, err := filepath.Rel(parentPath, localPath)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("unable to determine relative path of local file [%s]: %w", localPath, err)
 		}
 		relativePaths[i] = p
 	}
@@ -191,7 +191,7 @@ func ExtractFilePathsFromList(parentPath string, relativePaths []string) ([]stri
 	for _, p := range relativePaths {
 		paths, err := ExtractFilePaths(path.Join(parentPath, p))
 		if err != nil {
-			errs = append(errs, err)
+			errs = append(errs, fmt.Errorf("unable to extract file paths from [%s]: %w", p, err))
 		} else {
 			filePaths = append(filePaths, paths...)
 		}
@@ -206,7 +206,7 @@ func ExtractFilePaths(localPath string) ([]string, error) {
 	if iohelper.IsDirectoryExist(localPath) {
 		files, err := os.ReadDir(localPath)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("unable to read directory [%s]: %w", localPath, err)
 		}
 		filePaths := make([]string, 0)
 		for _, file := range files {
